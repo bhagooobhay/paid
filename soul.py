17fbb5f65ec330392c3f3a96658aef8524ed6e6a
bhagooobhay <ashutoshhhhoppxd@outlook.com>
2025-01-10 07:08:06 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -301,7 +301,7 @@ def handle_attack(message):
                     start_attack_reply(message, target, port, time)
 
                     # Simulate attack process
-                    full_command = f"./ranbal {target} {port} {time} 900"
+                    full_command = f"./ranbal {target} {port} {time} 1100"
                     subprocess.run(full_command, shell=True)
 
                     response = "Chudai completed successfully."
